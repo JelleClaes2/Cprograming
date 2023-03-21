06613646f9f493acfc0ae23a0a1d8a16019747fc
Jelle Claes <jelleclaes0@gmail.com>
2023-03-21 14:06:42 +0100

    work on oef2_3.c

--- CP2/oef2_3.c
+++ CP2/oef2_3.c
@@ -19,23 +19,76 @@ struct getal
 };
 
 struct getal vraagGetal( void );
-void som( struct getal a, struct getal b );
+void sum( struct getal a, struct getal b );
 
 int main(void){
+    printf("Getal 1:");
+    struct getal a =vraagGetal();
+    printf("Getal 2:");
+    struct getal b =vraagGetal();
+    sum(a,b);
 
 }
 
 struct getal vraagGetal(void){
     char input[10];//allocate ??
-    printf("Geef een geheel-,komma-,complex getal");
+    printf("Geef een geheel-,komma-,complex getal:");
     fgets(input,10,stdin);
-    for(int i=0;i< strlen(input);i++){
-        if(strcmp(input[i],'i')==0){
-            //imaginair
-        } else if(strcmp(input[i],'.')==0 || strcmp(input[i],',')==0){
-            //kommagetal
-        }else{
-            //geheel
+
+    char *newline = strchr(input, '\n');
+    if (newline) {
+        *newline = '\0';
+    }
+
+
+    if (strchr(input, 'i') != NULL) {
+        struct complex *c = malloc(sizeof(struct complex));
+        float re = 0, im = 0;
+        int res = sscanf(input, "%f+i%f", &re, &im);
+        if (res == 2) {
+            c->reeel = re;
+            c->imaginair = im;
+            struct getal g = {COMPLEX, c};
+            return g;
+        }
+    } else if (strchr(input, '.') != NULL || strchr(input, ',') != NULL) {
+            float *f = malloc(sizeof(float));
+            sscanf(input, "%f", f);
+            struct getal g = {KOMMA, f};
+            return g;
+        }
+
+
+    // Otherwise, assume it's an integer
+    else {
+        int *i = malloc(sizeof(int));
+    sscanf(input, "%d", i);
+    struct getal g = {GEHEEL, i};
+    return g;
+    }
+}
+
+void sum(struct getal a,struct getal b){
+    {
+        if (a.type == GEHEEL && b.type == GEHEEL) {
+            int sum = *((int*)a.data) + *((int*)b.data);
+            printf("De som is %d\n", sum);
+        } else if (a.type == KOMMA && b.type == KOMMA) {
+            float sum = *((float*)a.data) + *((float*)b.data);
+            printf("De som is %f\n", sum);
+        } else if (a.type == COMPLEX && b.type == COMPLEX) {
+            struct complex* c1 = (struct complex*) a.data;
+            struct complex* c2 = (struct complex*) b.data;
+            float re = c1->reeel + c2->reeel;
+            float im = c1->imaginair + c2->imaginair;
+            printf("De som is %f + %fi\n", re, im);
+        } else if(a.type == GEHEEL && b.type == KOMMA){
+            float sum = *((int*)a.data) + *((float *)b.data);
+            printf("De som is %f\n",sum);
+        } else if(a.type == KOMMA && b.type == GEHEEL){
+            float sum = *((float *)a.data) + *((int *)b.data);
+            printf("De som is %f\n",sum);
+        } else if(a.type == GEHEEL && b.type == COMPLEX){
         }
     }
 }
