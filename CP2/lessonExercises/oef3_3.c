b110181a9b35cacac73635c04bd990c6c6c615a5
Jelle Claes <jelleclaes0@gmail.com>
2023-06-12 13:56:09 +0200

    fucked around in oef3_3

--- CP2/lessonExercises/oef3_3.c
+++ CP2/lessonExercises/oef3_3.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-// Functie om een matrix af te drukken
 void printMatrix(int** matrix, int rows, int cols) {
     for (int i = 0; i < rows; ++i) {
         for (int j = 0; j < cols; ++j) {
@@ -12,10 +11,9 @@ void printMatrix(int** matrix, int rows, int cols) {
     }
 }
 
-// Functie om matrices op te tellen
 int** addMatrices(int** matrix1, int rows1, int cols1, int** matrix2, int rows2, int cols2) {
     if (rows1 != rows2 || cols1 != cols2) {
-        printf("Kan matrices niet optellen. Matrices moeten dezelfde afmetingen hebben.\n");
+        printf("Matrices kunnen niet worden opgeteld: onjuiste dimensies.\n");
         return NULL;
     }
 
@@ -30,10 +28,9 @@ int** addMatrices(int** matrix1, int rows1, int cols1, int** matrix2, int rows2,
     return result;
 }
 
-// Functie om matrices van elkaar af te trekken
 int** subtractMatrices(int** matrix1, int rows1, int cols1, int** matrix2, int rows2, int cols2) {
     if (rows1 != rows2 || cols1 != cols2) {
-        printf("Kan matrices niet aftrekken. Matrices moeten dezelfde afmetingen hebben.\n");
+        printf("Matrices kunnen niet worden afgetrokken: onjuiste dimensies.\n");
         return NULL;
     }
 
@@ -48,7 +45,6 @@ int** subtractMatrices(int** matrix1, int rows1, int cols1, int** matrix2, int r
     return result;
 }
 
-// Functie om een matrix met een scalaire waarde te vermenigvuldigen
 int** scalarMultiplyMatrix(int** matrix, int rows, int cols, int scalar) {
     int** result = (int**)malloc(rows * sizeof(int*));
     for (int i = 0; i < rows; ++i) {
@@ -61,10 +57,9 @@ int** scalarMultiplyMatrix(int** matrix, int rows, int cols, int scalar) {
     return result;
 }
 
-// Functie om matrices met elkaar te vermenigvuldigen
 int** multiplyMatrices(int** matrix1, int rows1, int cols1, int** matrix2, int rows2, int cols2) {
     if (cols1 != rows2) {
-        printf("Kan matrices niet vermenigvuldigen. Het aantal kolommen van matrix 1 moet gelijk zijn aan het aantal rijen van matrix 2.\n");
+        printf("Matrices kunnen niet worden vermenigvuldigd: onjuiste dimensies.\n");
         return NULL;
     }
 
@@ -82,45 +77,67 @@ int** multiplyMatrices(int** matrix1, int rows1, int cols1, int** matrix2, int r
     return result;
 }
 
+int** parseMatrix(char* str, int rows, int cols) {
+    int** matrix = (int**)malloc(rows * sizeof(int*));
+    char* token = strtok(str, " [;]");
+    int i = 0, j = 0;
+
+    while (token != NULL) {
+        if (strcmp(token, "]") == 0) {
+            break;
+        }
+        if (strcmp(token, ";") == 0) {
+            ++i;
+            j = 0;
+        } else {
+            matrix[i][j++] = atoi(token);
+        }
+        token = strtok(NULL, " [;]");
+    }
+
+    return matrix;
+}
+
+void freeMatrix(int** matrix, int rows) {
+    for (int i = 0; i < rows; ++i) {
+        free(matrix[i]);
+    }
+    free(matrix);
+}
+
 int main(int argc, char* argv[]) {
-    if (argc != 12) {
+   /* if (argc < 12) {
         printf("Ongeldige argumenten. Gebruik: ./main -m <rij1> <kolom1> \"[matrix1]\" <rij2> <kolom2> \"[matrix2]\" -b <bewerking>\n");
         return 1;
-    }
+    }*/
 
     int rows1 = atoi(argv[2]);
     int cols1 = atoi(argv[3]);
+    int rows2 = atoi(argv[5]);
+    int cols2 = atoi(argv[6]);
 
-    // Matrix 1
-    int** matrix1 = (int**)malloc(rows1 * sizeof(int*));
-    char* token = strtok(argv[4], "[];, ");
-    int count1 = 0;
-    while (token != NULL) {
-        if (count1 % cols1 == 0) {
-            matrix1[count1 / cols1] = (int*)malloc(cols1 * sizeof(int));
-        }
-        matrix1[count1 / cols1][count1 % cols1] = atoi(token);
-        token = strtok(NULL, "[];, ");
-        count1++;
+    if (rows1 <= 0 || cols1 <= 0 || rows2 <= 0 || cols2 <= 0) {
+        printf("Ongeldige rij- of kolomwaarden.\n");
+        return 1;
     }
 
-    int rows2 = atoi(argv[6]);
-    int cols2 = atoi(argv[7]);
+    /*if (argc != 8 + rows1 * cols1 + rows2 * cols2) {
+        printf("Ongeldige argumenten.\n");
+        return 1;
+    }*/
 
-    // Matrix 2
-    int** matrix2 = (int**)malloc(rows2 * sizeof(int*));
-    token = strtok(argv[8], "[];, ");
-    int count2 = 0;
-    while (token != NULL) {
-        if (count2 % cols2 == 0) {
-            matrix2[count2 / cols2] = (int*)malloc(cols2 * sizeof(int));
-        }
-        matrix2[count2 / cols2][count2 % cols2] = atoi(token);
-        token = strtok(NULL, "[];, ");
-        count2++;
-    }
+    char* matrix1Str = argv[4];
+    char* matrix2Str = argv[7 + rows1 * cols1];
+    char* operation = argv[8 + rows1 * cols1];
 
-    char* operation = argv[10];
+    /*if (strcmp(operation, "add") != 0 && strcmp(operation, "sub") != 0 &&
+        strcmp(operation, "scale") != 0 && strcmp(operation, "multiply") != 0) {
+        printf("Ongeldige bewerking.\n");
+        return 1;
+    }*/
+
+    int** matrix1 = parseMatrix(matrix1Str, rows1, cols1);
+    int** matrix2 = parseMatrix(matrix2Str, rows2, cols2);
 
     int** result = NULL;
 
@@ -129,33 +146,20 @@ int main(int argc, char* argv[]) {
     } else if (strcmp(operation, "sub") == 0) {
         result = subtractMatrices(matrix1, rows1, cols1, matrix2, rows2, cols2);
     } else if (strcmp(operation, "scale") == 0) {
-        int scalar = atoi(argv[11]);
+        int scalar = atoi(argv[8 + rows1 * cols1 + 1]);
         result = scalarMultiplyMatrix(matrix1, rows1, cols1, scalar);
     } else if (strcmp(operation, "multiply") == 0) {
         result = multiplyMatrices(matrix1, rows1, cols1, matrix2, rows2, cols2);
-    } else {
-        printf("Ongeldige bewerking.\n");
     }
 
     if (result != NULL) {
+        printf("Matrixresultaat:\n");
         printMatrix(result, rows1, cols2);
-
-        // Geheugen vrijgeven
-        for (int i = 0; i < rows1; ++i) {
-            free(matrix1[i]);
-        }
-        free(matrix1);
-
-        for (int i = 0; i < rows2; ++i) {
-            free(matrix2[i]);
-        }
-        free(matrix2);
-
-        for (int i = 0; i < rows1; ++i) {
-            free(result[i]);
-        }
-        free(result);
+        freeMatrix(result, rows1);
     }
+
+    freeMatrix(matrix1, rows1);
+    freeMatrix(matrix2, rows2);
 
     return 0;
 }
